c7713dc48737379f7e64c0e49182fafccdeeb7e2
Griffy <geyf6@mail2.sysu.edu.cn>
2024-05-22 15:15:46 +0800

    Update Scripts and README.md file.

--- README.md
+++ README.md
@@ -6,7 +6,19 @@ This repo contains some R scripts for visualzing data.
 2. Volcano plot
 3. Ordination analysis plot
 
-**Note**: 
+---
+
+**Note:**
 
 - For heatmap, [TBtools](https://github.com/CJ-Chen/TBtools-II/releases) is highly recommended;
 - For bar plot with error bar and significant difference, GraphPad Prism is highly recommended.
+
+
+
+## Bubble Plot
+
+Bubble plot is often used to illustrate the result of KEGG enrichment analysis. Here, an outcome of KEGG enrichment is used as the example data.
+
+**Final plot:**
+
+![Image](./Example_file/bubble_plot.png)
--- Scripts/bubble_plot.R
+++ Scripts/bubble_plot.R
@@ -1,14 +1,18 @@
+# 1. Packages preparation ====
 library(tidyverse)
 library(rio)
 
-kegg_enrich <- import("kegg_data.xlsx", sheet = 6)
+# 2. Data import ====
+kegg_enrich <- import("Example_file/bubble_plot_data.xlsx")
 
+# 3. Data visualization ====
 ggplot(data = kegg_enrich,
-       mapping = aes(x = kegg_enrich$`out (109)`/kegg_enrich$`All (1250)`,
-                     y = reorder(kegg_enrich$Pathway, kegg_enrich$`out (109)`/kegg_enrich$`All (1250)`))) +
-  geom_point(aes(size = kegg_enrich$`out (109)`,
-                 color = kegg_enrich$Qvalue)) +
-  scale_size(range = c(4,10), breaks = seq(10,50,20)) +
+       mapping = aes(x = interest_gene_set/background_gene,
+                     y = reorder(Pathway, interest_gene_set/background_gene))) +
+  geom_point(aes(size = interest_gene_set,
+                 color = Qvalue)) +
+  scale_size(range = c(4,10), 
+             breaks = seq(10,50,20)) +
   scale_color_gradient(low = "#e84133",
                        high = "#1f78b4") +
   scale_x_continuous(limits = c(-0.1, 1.1),
@@ -17,14 +21,9 @@ ggplot(data = kegg_enrich,
        y = "KEGG Pathway",
        size = "Gene Number",
        color = "qvalue") +
-  facet_grid(rows = vars(kegg_enrich$group),scales = "free",space = "free",
+  facet_grid(rows = vars(kegg_enrich$group),
+             scales = "free",space = "free",
+             #as.table -> change the relative position of subplots
+             #Here, "down" is the upper panel by default. -> Set: as.table = FALSE -> "down" becomes the bottom panel
              as.table = FALSE) +
   theme_bw()
-  
-
-theme(panel.grid = element_blank())
-
-
-geom_text(aes(label = sprintf("%0.2f", 
-                                GeneRatio_in_background),
-                x = GeneRatio_in_background + 20))
